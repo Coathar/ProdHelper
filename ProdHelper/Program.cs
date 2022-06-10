cd8db8eafd805637d2e54f7a69a7392e2ee175b8
Coathar <trainman1234@gmail.com>
2022-06-10 00:42:18 -0500

    Add project files.

---
+++ ProdHelper/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace ProdHelper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("ProdHelper Utility");
+            Console.WriteLine("Written by Coathar");
+            Console.WriteLine("");
+            ListOptions();
+
+            while (!endApp)
+            {
+                string? key = Console.ReadLine();
+
+                if (key != null)
+                {
+                    switch (key.ToLower())
+                    {
+                        case "1":
+                            SettingsFileUtil.Intitialize();
+                            break;
+                        case "h":
+                            ListOptions();
+                            break;
+                        case "x":
+                            endApp = true;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option, enter 'H' for help.");
+                            break;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Goodbye");
+        }
+
+        public static void ListOptions()
+        {
+            Console.WriteLine("Options:");
+            Console.WriteLine("1: Settings Util");
+            Console.WriteLine("H: List this menu");
+            Console.WriteLine("X: Close");
+        }
+    }
+}
---
+++ ProdHelper/SettingsFileUtil.cs
@@ -0,0 +1,109 @@
+﻿namespace ProdHelper
+{
+    public class SettingsFileUtil
+    {
+        public static void Intitialize()
+        {
+            string settingsFolder = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\\Documents\\Overwatch\\Settings";
+            string[] templateFiles = new string[0];
+            string selectedFile = "";
+
+            bool validFolder = false;
+            bool validTemplate = false;
+
+            while (!validFolder)
+            {
+                Console.WriteLine($"Enter settings folder path (push enter for: {settingsFolder})");
+                string? input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    settingsFolder = input;
+                }
+
+                if (!Directory.Exists(settingsFolder))
+                {
+                    Console.WriteLine("Invalid settings folder specified");
+                    continue;
+                }
+
+                if (!Directory.Exists(settingsFolder + "\\Templates"))
+                {
+                    Console.WriteLine("No templates folder found, creating one now. After this, add in your template files to use.");
+                    Directory.CreateDirectory(settingsFolder + "\\Templates");
+                    return;
+                }
+
+                templateFiles = Directory.GetFiles(settingsFolder + "\\Templates");
+
+                if (templateFiles.Length == 0)
+                {
+                    Console.WriteLine($"No template files found. Please add some to {settingsFolder}\\Templates");
+                    return;
+                }
+
+                validFolder = true;
+            }
+
+            while (!validTemplate)
+            {
+                Console.WriteLine("Select a template file to use:");
+
+                int index = 1;
+
+                foreach (string s in templateFiles)
+                {
+                    Console.WriteLine($"{index++}. {Path.GetFileName(s)}");
+                }
+
+                string? indexInput = Console.ReadLine();
+
+                if (int.TryParse(indexInput, out int selectedIndex))
+                {
+                    if (selectedIndex > templateFiles.Length)
+                    {
+                        Console.WriteLine("Invalid selection number.");
+                        continue;
+                    }
+
+                    selectedFile = templateFiles[selectedIndex - 1];
+                    break;
+                }
+
+                Console.WriteLine("Invalid selection number.");
+            }
+
+            while (true)
+            {
+                Console.WriteLine($"Would you like to move {Path.GetFileName(selectedFile)} to your settings folder? This will OVERRIDE your current settings file! (y/n)");
+
+                string? key = Console.ReadLine();
+
+                if (key != null)
+                {
+                    if (key.ToLower() == "y")
+                    {
+                        try
+                        {
+                            File.Copy(selectedFile, settingsFolder + "\\Settings_v0.ini", true);
+                            Console.WriteLine($"Loaded settings file {Path.GetFileName(selectedFile)}! Press any key to continue.");
+                            Console.ReadKey();
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Error writing file to folder, aborting.");
+                            break;
+                        }
+                    }
+                    else if (key.ToLower() == "n")
+                    {
+                        Console.WriteLine("Aborting settings loading.");
+                        break;
+                    }
+                }
+                
+            }
+        }
+    }
+}
